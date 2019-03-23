2dbbee30691b1da6c657152d358e1873369f4f16
WHBJ <bevansrealtors@gmail.com>
2019-03-22 21:58:58 -0500

    unfinished app push

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,38 @@
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model (cat.js) to use its database functions.
+var burger = require("../models/burger.js");
+
+router.get("/", function (req, res) {
+    burger.selectAll(function (data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post('/burgers', function (req, res) {
+    burger.insertOne([
+        'burger_name'
+    ], [
+        req.body.burger_name
+    ], function (data) {
+        res.redirect('/');
+    });
+});
+
+router.put('/burgers/:id', function (req, res) {
+    var condition = 'id = ' + req.params.id;
+
+    burger.updateOne({
+        devoured: true
+    }, condition, function (data) {
+        res.redirect('/');
+    });
+});
+
+module.exports = router;
